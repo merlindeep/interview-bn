58ebc824dd98698852cff35d41b5b2004642e539
merlindeep <merlindeep@gmail.com>
2014-01-14 18:40:40 +0000

    add task text to about page

--- protected/views/site/pages/about.php
+++ protected/views/site/pages/about.php
@@ -7,3 +7,13 @@ $this->breadcrumbs = array(
 );
 ?>
 <h1>About</h1>
+
+<h3>Task</h3>
+
+<p>Реализовать Autocomplete, используя:</p>
+<ol>
+    <li>PHP 5, можно задействовать любой framework (Yii, CodeIgniter, Symfony и т.д.)</li>
+    <li>Jquery, можно использовать любые готовые компоненты</li>
+    <li>Данные брать из MySQL</li>
+    <li>Если был опыт работы со Sphinx, то задействовать его.</li>
+</ol>
